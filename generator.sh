d68d470e19ad09978ff20e13d4a3720c2e8318e7
Frederic Leger <frederic@webofmars.com>
2020-05-15 00:45:41 +0200

    FIX: docker push not working

--- generator.sh
+++ generator.sh
@@ -3,5 +3,5 @@
 while read VERSION; do
   echo "+ building "
   docker build --pull -t $IMAGE_NAME:$VERSION --build-arg ASDF_VERSION=$VERSION .
-  echo docker push $IMAGE_NAME:$VERSION
+  docker push $IMAGE_NAME:$VERSION
 done < <(cat versions.txt)
