39844d4f7b71b5ff481ec4b760554afb7045baf3
Frederic Leger <frederic@webofmars.com>
2020-05-14 17:03:35 +0200

    d'ont break on 1st image built

--- generator.sh
+++ generator.sh
@@ -4,5 +4,4 @@ while read VERSION; do
   echo "+ building "
   docker build --pull -t $IMAGE_NAME:$VERSION --build-arg ASDF_VERSION=$VERSION .
   echo docker push $IMAGE_NAME:$VERSION
-  exit 0
 done < <(cat versions.txt)
