47a0372ca1593aebeda43db37717678e975c4286
Frederic Leger <frederic@webofmars.com>
2021-10-06 23:54:26 +0200

    better logic for versions handling and migrated travis

--- generate-versions-list.sh
+++ generate-versions-list.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 # safer than rm
 echo > versions.txt
 
-while read VERSION; do
-  echo "+ testing version $VERSION localy"
-  if docker build --pull -t $IMAGE_NAME:$VERSION --build-arg ASDF_VERSION=$VERSION .; then
-    echo $VERSION >> versions.txt
-  fi
-  docker image rm $IMAGE_NAME:$VERSION
+while read -r VERSION; do
+  echo "${VERSION}" >> versions.txt
 done < <(git ls-remote https://github.com/asdf-vm/asdf.git | grep -o -E 'refs/tags/v[A-Za-z0-9\.-]*' | sed 's:refs/tags/::' | sort -rV)
