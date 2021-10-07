04ef3b7c023b62d48503ddb05b53613adc689be3
Frederic Leger <frederic@webofmars.com>
2021-10-07 22:07:42 +0200

    better scripts and travis upgrade

--- generator.sh
+++ generator.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-while read VERSION; do
-  echo "+ building "
+set -eu -o pipefail
+
+VERSIONS="${1:-versions.txt}"
+
+while read -r VERSION; do
+  echo "+ building version ${VERSION}"
   docker build --pull -t $IMAGE_NAME:$VERSION --build-arg ASDF_VERSION=$VERSION .
   docker push $IMAGE_NAME:$VERSION
-done < <(cat versions.txt)
+done < "${VERSIONS}"
